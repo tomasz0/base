1c4467b6938f58598fcaa8de8c41a5e64d744a1e
Tomasz Niedzwiedzki <tom.niedzwiedzki@gmail.com>
2019-06-17 00:07:53 +0200

    -n

---
+++ build/CMakeFiles/Database.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tomasz/C++/StudentsDatabase_Luki/Database.cpp" "/home/tomasz/C++/StudentsDatabase_Luki/build/CMakeFiles/Database.dir/Database.cpp.o"
+  "/home/tomasz/C++/StudentsDatabase_Luki/Employee.cpp" "/home/tomasz/C++/StudentsDatabase_Luki/build/CMakeFiles/Database.dir/Employee.cpp.o"
+  "/home/tomasz/C++/StudentsDatabase_Luki/Person.cpp" "/home/tomasz/C++/StudentsDatabase_Luki/build/CMakeFiles/Database.dir/Person.cpp.o"
+  "/home/tomasz/C++/StudentsDatabase_Luki/PeselValidation.cpp" "/home/tomasz/C++/StudentsDatabase_Luki/build/CMakeFiles/Database.dir/PeselValidation.cpp.o"
+  "/home/tomasz/C++/StudentsDatabase_Luki/Student.cpp" "/home/tomasz/C++/StudentsDatabase_Luki/build/CMakeFiles/Database.dir/Student.cpp.o"
+  "/home/tomasz/C++/StudentsDatabase_Luki/main.cpp" "/home/tomasz/C++/StudentsDatabase_Luki/build/CMakeFiles/Database.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Database.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Database.dir/Database.cpp.o"
+  "CMakeFiles/Database.dir/Person.cpp.o"
+  "CMakeFiles/Database.dir/Employee.cpp.o"
+  "CMakeFiles/Database.dir/Student.cpp.o"
+  "CMakeFiles/Database.dir/PeselValidation.cpp.o"
+  "CMakeFiles/Database.dir/main.cpp.o"
+  "Database.pdb"
+  "Database"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Database.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
